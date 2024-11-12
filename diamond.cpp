171f893aba9e44a72cac9000aaba146b2741aac9
pouriya <pouriyahmn@gmail.com>
2024-11-12 14:41:15 +0330

    update

--- diamond.cpp
+++ diamond.cpp
@@ -13,4 +13,15 @@ int main(){
                 cout << endl;
                 
     }
+    for ( int i = 10; i > 2; i=i-2)
+    {
+        cout <<setw(14-i);
+        for ( int j = 5; j < i; j++)
+        {
+            cout<<"* ";
+        }
+        cout << endl;
+        
+    }
+    
 }
