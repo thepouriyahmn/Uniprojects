c5b5279aa5c866811d574c907a8084cf4c4ac9e8
pouriya <pouriyahmn@gmail.com>
2024-11-12 14:08:08 +0330

    update

--- diamond.cpp
+++ diamond.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 #include <iomanip>
+using namespace std;
 int main(){
-    
+
+     for (int i = 1; i < 9; i= i+2)
+    {
+        cout << setw(9-i);
+                for (int j = 1; j <= i; j++)
+                {
+                    cout << "* ";
+                }
+                cout << endl;
+                
+    }
 }
